15e3af707bc3a0cfa0f4821a25fb732965fd649c
koftepatates <147235304+koftepatates@users.noreply.github.com>
2023-10-07 10:10:18 +0000

    bugun cok guzel eyler ogrendim kofte patattes

--- kofteler sevin.py
+++ kofteler sevin.py
@@ -1,1 +1,18 @@
-print('python ile ilk günüm')
+# print('python ile ilk günüm')
+# print('naberkofteli\nnaberkofteli\naberkofteli')
+# print('merhaba' +' '+ 'koftepatates')
+# print('merhaba' + ' '+ 'koftepatates') 
+# print (
+#     input('merhaba, adın nedir')
+# )
+#print(len("Kofte"))
+#print(len("Kofte"))
+
+#print(len(input("merhaba,adın nedir ")))
+#print(input("merhaba,adın nedir?"))
+
+#isim = input("merhaba adın nedir")
+#print(isim)
+#print("merhaba "+isim)
+#isim = "KoftePatates"
+#print(isim)
