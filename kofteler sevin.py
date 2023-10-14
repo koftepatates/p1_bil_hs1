a2bef56f045d2a97a8c41fc44472bbfd67a4df86
koftepatates <147235304+koftepatates@users.noreply.github.com>
2023-10-14 09:58:01 +0000

    kofte

--- kofteler sevin.py
+++ kofteler sevin.py
@@ -1,7 +1,7 @@
 # print('python ile ilk günüm')
 # print('naberkofteli\nnaberkofteli\naberkofteli')
 # print('merhaba' +' '+ 'koftepatates')
-# print('merhaba' + ' '+ 'koftepatates') 
+# print('merhaba' + ' '+ 'koftepatates')
 # print (
 #     input('merhaba, adın nedir')
 # )
@@ -16,3 +16,83 @@
 #print("merhaba "+isim)
 #isim = "KoftePatates"
 #print(isim)
+# print(len(input("merhaba adın nedir?")))
+# isim=input("merhaba adın nedir?")
+#uzunluk=len(isim)
+# print(uzunluk)
+# ensongidilensehiradi = input("En son hangi şehire gittiniz?")
+# evcilhayvanadı = input("Evcil hayvanınızın adı nedir?")
+# print("grup adı: " + " " + ensongidilensehiradi + " "+evcilhayvanadı)
+# sayi1 = input("1nci sayıyı giriniz: ")
+# sayi2 = input("2nci sayıyı giriniz: ")
+# toplam = int(sayi1) + int(sayi2)
+# print("Toplam = " + str(toplam))
+# kısakenar = input("kısa kenarı giriniz: ")
+# uzunkenar = input("uzun kenarı getiriniz: ")
+# cevre = (int(kısakenar) + int(uzunkenar)) * 2
+# alan = int(kısakenar) * int(uzunkenar)
+# print("Dikdörtgenin Çevresi: " + str(cevre))
+# print("Dikdörtgenin Alanı: " + str(alan))
+# yaricap = int(input("Yarıçapı giriniz: "))
+# yukseklik = int(input("Yüksekliği giriniz: "))
+# pisayisi = 3
+# tabanAlani = pisayisi * yaricap * yaricap
+# tabanCevresi = 2 * pisayisi * yaricap
+# yanalAlan = yukseklik * tabanCevresi
+# toplamAlan = yanalAlan + (2 * tabanAlani)
+# hacim = tabanAlani * yukseklik
+# print("Silindirin hacmi: " + str(hacim))
+# print("Silindirin alanı: " + str(toplamAlan))
+# yaricap = int(input("Yarıçapı giriniz: "))
+# pisayisi = 3
+# cevre = 2 * pisayisi * yaricap
+# hacim = pisayisi * yaricap * yaricap
+# print("Dairenin hacmi: " + str(hacim))
+# print("Dairenin çevresi: " + str(cevre))
+# print(len(str(1234567890)))
+# print("Merhaba" [0])
+# print(123 + 456)
+# print(123_456_789)
+# isimUzunluk = len(input("Merhaba, adın ne?: "))
+# isimUzunluk_Str = str(isimUzunluk)
+# print("İsminde " + isimUzunluk_Str + "karakter var.")
+# fl = float(1.322)
+# print(type(fl))
+# intt = int(232)
+# print(type(intt))
+# st = str(123123)
+# print(type(st))
+# print(100 + float(15.23))
+# print(str(122) + str(324))
+# yas = input("Kaç yaşındasın?")
+# yas_int = int(yas)
+# ay = yas_int * 12
+# hafta = yas_int * 52
+# gun = yas_int * 365
+# print(f"{ay} aylıksın")
+# print(f"{hafta} haftalıksın")
+# print(f"{gun} günlüksün")
+# print("Lunaparka hoşgeldiniz!")
+# print("Bilet fiyatı yetişkinler için 30 tl, 10 yaşından küçükler için 18 tl")
+# boy = int(input("Lütfen boyunuzu giriniz: (cm)"))
+# yas = int(input("Lütfen yaşınızı giriniz: "))
+# biletfiyati = 0
+# if boy > 100 :
+#     if(yas<10):
+#         biletfiyati = 18
+#     else:
+#         biletfiyati = 30
+#     print(f"Crazy Dance'a binebilirsiniz! Fiyat {biletfiyati} TL") 
+# elif boy > and boy < 100
+#     if(yas < 10):
+#         bilatfiyati = 18
+#     else:,
+#         biletfiyati = 30
+#     print("Hız trenine binebilirsiniz! Fiyat {biletfiyati}  TL")
+# elif boy > 100 and boy < 150:
+#     if(yas < 10):
+#         biletfiyati = 18
+# else:
+#     print("Lunaparktaki hic bir alete boyunuz yetmiyor")
+
+
