94767e77b21c68cc5ea39a398d94b1a5b01a71b8
koftepatates <147235304+koftepatates@users.noreply.github.com>
2023-10-21 09:49:13 +0000

    bugun 3 tane efsanevi cıkardım biti tomruk ikisi mega sovalye

--- kofteler sevin.py
+++ kofteler sevin.py
@@ -96,3 +96,230 @@
 #     print("Lunaparktaki hic bir alete boyunuz yetmiyor")
 
 
+# sayi = int(input("Lütfen tek mi çift mi olduğunu öğrenmek için bir sayı giriniz."))
+# kalan = sayi % 2
+# if kalan ==0:
+#         print("Girdiğiniz sayı bir çift sayıdır!")
+# else:
+#         print("Girdiğiniz sayı bir tek sayıdır!")
+# boy = float(input("Boyunuzu giriniz (m): "))
+# kilo = float(input("Kilonuzu giriniz (kg): "))
+
+# vki = int(kilo/boy**2)
+# if vki < 18.5:
+#     print(f"Vücut kütle indeksiniz: {vki} , Zayıfsınız!")
+# elif vki > 18.5 and vki < 25:
+#     print(f"Vücut kütle indeksiniz: {vki} , Kilonuz Normal!")
+# elif vki > 25 and vki < 30:
+#     print(f"Vücut kütle indeksiniz: {vki} , Kilolusunuz!")
+# elif vki > 30 and vki < 35:
+#     print(f"Vücut kütle indeksiniz: {vki} , Obezsiniz!")
+# elif vki > 35:
+#     print(f"Vücut kütle indeksiniz: {vki} , Aşırı Obezsiniz!")
+
+# yil = int(input("Hangi yılı kontrol etmek istiyorsunuz?"))
+# if yil % 4 == 0:
+#     if yil % 100 == 0:
+#         if yil % 400 == 0:
+#             print(f"{yil} bir artık yıldır!")
+#         else:
+#             print(f"{yil} bir artık yıl değildir!.")
+#     else:
+#          print(f"{yil} bir artık yıl değildir!.")
+# else:
+#     print(f"{yil} bir artık yıl değildir!.")
+# boy = input("Mezitli Pizzaya hoşgeldiniz! Hangi Boy pizza istersiniz? K veya O veya B =>")
+
+# ekstraSos = input("Ekstra sos ister misiniz? E veya H =>")
+# ekstraPeynir = input("Ekstra peynir ister misiniz? E veya H =>")
+# toplam = 0 
+# if boy == "K":
+#     toplam += 15
+# elif boy == "O":
+#     toplam += 20 
+# elif boy == "B":
+#     toplam += 25
+
+# if ekstraSos == "E":
+#     if boy == "K":
+#         toplam += 2
+#     else:
+#         toplam += 5
+# if ekstraPeynir == "E":
+#     toplam += 3
+
+# print(f"Toplam Ödeme = {toplam}")
+
+# print("Sevgi Hesaplayıcıya hoşgeldiniz!")
+# isim1 = input("Adınız nedir?")
+# isim2 = input("Onun adı nedir?")
+
+# isimler = ( isim1 + isim2 ).lower()
+
+# g = isimler.count("g")
+# e = isimler.count("e")
+# r = isimler.count("r")
+# ç = isimler.count("ç")
+# e = isimler.count("e")
+# k = isimler.count("k")
+# a = isimler.count("a")
+# ş = isimler.count("ş")
+# k = isimler.count("k")
+
+# toplam = g + e + r + ç + e + k + a + ş + k
+
+# if( toplam < 10 ):
+#     print(f"Sevgi puaınınız: {toplam}, kane ve aj lee  gibisiniz!")
+# elif( toplam > 10):
+#     print(f"Sevgi puanınız: {toplam}, seth ve becky  gibisiniz!")
+# else:
+#     print(f"Sevgi puanınız: {toplam}")
+#     print('''
+# ___________                        _________                      __  .__    .__                 
+# \__    ___/__.__.______   ____    /   _____/ ____   _____   _____/  |_|  |__ |__| ____    ____   
+#   |    | <   |  |\____ \_/ __ \   \_____  \ /  _ \ /     \_/ __ \   __\  |  \|  |/    \  / ___\  
+#   |    |  \___  ||  |_> >  ___/   /        (  <_> )  Y Y  \  ___/|  | |   Y  \  |   |  \/ /_/  > 
+#   |____|  / ____||   __/ \___  > /_______  /\____/|__|_|  /\___  >__| |___|  /__|___|  /\___  /  
+#           \/     |__|        \/          \/             \/     \/          \/        \//_____/   
+# ''')
+# yas = int(input("Kaç yaşındasınız"))
+# if yas >= 18:
+#     saglık = input("Sağlıklı mısın? e veya h =>")
+#     if saglık == "e":
+#         print("Ehliyet alabilirin")
+#     else:
+#         print("Üzgünüm, ehliyet alamzsın")
+# else:
+#     print("Üzgünüm, ehliyet alamzsın")
+# sifre = "005162"
+# print("Mezitli Bankasına Hoşgeldiniz!")
+# sifreGirilen = input("Lütfen şifrenizi giriniz!")
+# print(sifreGirilen)
+# if len(sifreGirilen) != 6:
+#     print("Eksik ya da fazla tuşladınız!")
+# elif sifre == sifreGirilen:
+#     print("Hoşgeldiniz!")
+# else:
+#     print("Yanlış tuşladınız!")
+# import math
+# boy = float(input("Boyunuzu metre cinsinden giriniz!"))
+# kilo = int(input("Kilonuzu kilogram cinsinden giriniz!"))
+
+# idealKilo = math.floor(22*boy*boy)
+# fark = abs(idealKilo - kilo)
+# if idealKilo > kilo:
+#     print(f"{fark} kilo almalısınız!")
+# else:
+#     print(f"{fark} kilo vermelisiniz!")
+# sayi1 = int(input("1nci Sayıyı Giriniz!"))
+# sayi2 = int(input("2nci Sayıyı Giriniz!"))
+# sayi3 = int(input("3nci Sayıyı Giriniz!"))
+# sayi4 = int(input("4nci Sayıyı Giriniz!"))
+# sayi5 = int(input("5nci Sayıyı Giriniz!"))
+# enBuyuk = sayi1
+
+# if enBuyuk < sayi2:
+#     enBuyuk  = sayi2
+# if enBuyuk < sayi3:
+#     enBuyuk  = sayi3
+# if enBuyuk < sayi4:
+#     enBuyuk  = sayi4
+# if enBuyuk < sayi5:
+#     enBuyuk  = sayi5 
+
+# print(f"Girilen Sayılardan En Büyüğü = {enBuyuk}")
+# import random
+
+# randomSayi = random.randint(1,3)
+# cevap = int(input("Taş (1), Kağıt (2)  Makas (3) Seçiniz"))
+sehirler = ["Adana","Adıyaman","Afyonkarahisar","Ağrı","Aksaray","Amasya","Ankara","Antalya","Ardahan","Artvin","Aydın","Balıkesir","Bartın","Batman",
+"Bayburt","Bilecik","Bingöl","Bitlis","Bolu","Burdur","Bursa","Çanakkale","Çankırı","Çorum","Denizli","Diyarbakır","Düzce","Edirne","Elazığ",
+"Erzincan","Erzurum","Eskişehir","Gaziantep","Giresun","Gümüşhane","Hakkari","Hatay","Iğdır","Isparta","İstanbul","İzmir","Kahramanmaraş","Karabük",
+"Karaman","Kars","Kastamonu","Kayseri","Kilis","Kırıkkale","Kırklareli","Kırşehir","Kocaeli","Konya","Kütahya",
+"Malatya","Manisa","Mardin","Mersin","Muğla","Muş","Nevşehir","Niğde","Ordu","Osmaniye","Rize","Sakarya","Samsun","Şanlıurfa",
+"Siirt","Sinop","Şırnak","Sivas","Tekirdağ","Tokat","Trabzon","Tunceli","Uşak","Van","Yalova","Yozgat","Zonguldak"]
+# print(sehirler[0])
+# print(sehirler.index("Adana"))
+# print(sehirler[12])
+# print(sehirler[80])
+# print(sehirler[-1]) 
+# print(sehirler[-2]) 
+# sehirler[33] = "İçel"
+# print(sehirler[33])
+
+# sehirler.append("Tarsus")
+# print(sehirler[81])
+# print(len(sehirler))
+# sehirler.remove("Tarsus")
+# print(len(sehirler))
+# sehirler.pop()
+# print(len(sehirler))
+# import random
+# isimler = "Gekkoberk, Met, Sedo, Halinur, Xagan, Cemokero"
+# isimDizisi = isimler.split(", ")
+# index = random.randint(0, len(isimDizisi) - 1 )
+# print(f"Hesabı {isimDizisi[index]} ödeyecek!")
+# row1 = ['⬜️', '⬜️', '⬜️']
+# row2 = ['⬜️', '⬜️', '⬜️']
+# row3 = ['⬜️', '⬜️', '⬜️']
+# harita = [row1, row2, row3]
+# print(f"{row1}\n{row2}\n{row3}\n")
+
+# pozisyon = input("Hazineyi nereye koymak istersin?\n")
+
+# yatay = int(pozisyon[0]) -1
+# dikey = int(pozisyon[1]) - 1 
+# harita[dikey][yatay] = "X"
+# print(f"{row1}\n{row2}\n{row3}\n")
+# import random
+# taş = '''
+#     _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)
+# '''
+
+# kağıt = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#           _______)
+#          _______)
+# ---.__________)
+# '''
+
+# makas = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+# '''
+# while True:
+#     kullaniciSecimi = int(input("Taş(0), Kağıt(1), Makas(2) seç!"))
+#     bilgisayarSecimi = random.randint(0,2)
+
+#     print("Senin Seçimin")
+#     if kullaniciSecimi == 0:
+#         print(taş)
+#     elif kullaniciSecimi == 1:
+#         print(kağıt)
+#     elif kullaniciSecimi == 2:
+#         print(makas)
+
+
+#         if kullaniciSecimi >=3 or kullaniciSecimi < 0:
+#             print("0 , 1 ,veya 2 giriniz!")
+#     elif kullaniciSecimi == 0 and bilgisayarSecimi == 2:
+#         print("Yendin!")
+#     elif bilgisayarSecimi == 0 and kullaniciSecimi == 2:
+#         print("Kaybettin!")
+#     elif kullaniciSecimi > bilgisayarSecimi:
+#         print("Yendin!")
+#     elif bilgisayarSecimi > kullaniciSecimi:
+#         print("Kaybettin!")
+#     else:
+#         print("Berabere!")
